a82b8282b874ea68e3f5ba8cf2c08dbe6e12caf8
Asif Mahmud <amahmud@uci.edu>
2017-08-14 23:25:54 -0700

    updated Detector component

--- components/Detector.js
+++ components/Detector.js
@@ -13,3 +13,23 @@ import NativeModules, {ImagePickerManager} from 'NativeModules';
 import Button from './Button';
 import RNFetchBlob from 'react-native-fetch-blob';
 import _ from lodash;
+
+export default class Detector extends Component{
+    // Constructor that is executted before the component is mounted
+    constructor(props){
+        super(props);
+        this.state = {
+            // Setting the default photo style
+            photo_style: {
+                position: 'relative',
+                width: 480,
+                height: 480
+            },
+            // Boolean value that is used to determine whether to show the "Detect Faces" button
+            has_photo = false,
+            photo: null,
+            // Used as the data source for constructing face boxes
+            face_data: null
+        }
+    }
+}
